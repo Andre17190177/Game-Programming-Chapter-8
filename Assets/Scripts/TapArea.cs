7c23003076aa28daac9d770f1b03652a21394828
DESKTOP-4HPAV50\ASUS <andreas1439setiawan@gmail.com>
2021-10-02 16:35:58 +0700

    Upload Tugas Chapter 8

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    private AudioSource click;
+    public void OnPointerDown(PointerEventData eventData)
+
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+        click.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        click = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
